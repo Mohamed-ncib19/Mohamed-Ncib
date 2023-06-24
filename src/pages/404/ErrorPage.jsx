55c23c3b01f92a837d97278d79815df601ef68bd
Mohamed-ncib19 <mohamedncib900@gmail.com>
2023-06-24 05:06:44 +0100

    Updates

---
+++ src/pages/404/ErrorPage.jsx
@@ -0,0 +1,32 @@
+import { useNavigate  } from "react-router-dom";
+
+import Navbar from "../navbar/navbar";
+import Footer from "../footer/footer";
+const ErrorPage = () => {
+    const Navigate = useNavigate();
+    return ( 
+        <div className="bg-[#0F0F0F] h-screen  text-white">
+        <div className="bg-[#6947ef] w-[40%] h-[20vh] rounded-full blur-3xl m-auto  lg:w-[22%] transition-all duration-500">
+          <p></p>
+        </div>
+        <Navbar />
+        <div id="content" className="flex flex-col justify-center items-center m-auto gap-12 mb-28">
+            <div className="flex flex-col justify-center items-center m-auto">
+                <h1 className="text-[100px]">404</h1>
+                <h1 className="text-[80px]">Page Not Found</h1>
+                <p>The Page You Are Looking for is doesnt exist</p>
+            </div>
+            <button
+            className="flex flex-row justify-center items-center text-center m-auto lg:w-[15%] w-[50%] p-2 rounded-xl bg-[#6947ef] text-white hover:bg-[#6947ef] font-medium hover:text-[#0F0F0F] transition-all duration-500"
+            onClick={()=>{
+                Navigate('/Mohamed-Ncib');
+            }}
+            >Explore Home</button>
+            
+        </div>
+        <Footer />
+        </div>
+     );
+}
+ 
+export default ErrorPage;
--- src/pages/Home/App.jsx
+++ src/pages/Home/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
 import "../../css/index.css";
 import HomePage from "./HomePage";
 import SlideBar from "../navbar/slideBar";
@@ -6,18 +7,28 @@ import About from "../About/About.jsx";
 import Works from "../Works/Works.jsx";
 import Contact from "../Contact/Contact.jsx";
 import Thanks from "../Contact/Thanks";
+import ErrorPage from "../404/ErrorPage";
+
+function AppContent() {
+  const location = useLocation();
+
+  return (
+    <Routes location={location}>
+      <Route path="/Mohamed-ncib" element={<HomePage />} />
+      <Route path="/Slide-Bar" element={<SlideBar />} />
+      <Route path="/About-Me" element={<About />} />
+      <Route path="/Works" element={<Works />} />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/Thank-you" element={<Thanks />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/Mohamed-ncib" exact element={<HomePage />} />
-        <Route path="/Slide-Bar" element={<SlideBar />} />
-        <Route path="/About-Me" element={<About />} />
-        <Route path="/Works" element={<Works />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Thank-you" element={<Thanks />} />
-      </Routes>
+      <AppContent />
     </BrowserRouter>
   );
 }
