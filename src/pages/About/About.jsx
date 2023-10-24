fc370d2b0a75bd1ab6b555edcf39e446de46fb35
Mohamed-ncib19 <mohamedncib900@gmail.com>
2023-10-24 22:55:59 +0100

    add dialog section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,10 +6,9 @@ import Footer from "../footer/footer";
 import ScrollToTop from "../../functions/scrollToTp";
 import Icons from "../icons/Icons";
 import FrontEndIcon from "../icons/FrontEndIcon";
-import DownloadIcon from "../icons/download.jsx"
 import MohamedPicture from "../../assets/ME.png"
 import PlaceHolderImage from "../../assets/ME_LowRes.png"
-import Resume from '../../assets/PDF/Resume2023-2024.pdf'
+import DialogBox from "./Dialog" 
 
 const About = () => {
   ScrollToTop();
@@ -24,14 +23,7 @@ const About = () => {
             <div className="lg:w-[40%] w-[70%] gap-8 flex flex-col justify-center items-center text-center">
                 <h2 className="text-[40px] tracking-wider">Mohamed Ncib</h2>
                 <p className="tracking-wider w-full text-[16px]">Hello world , Im a computer Science Student , Web Designer , Web Developer with a passion for creating functional and user-friendly websites.</p>
-               <a
-                href={Resume}
-                download="Mohamed-Ncib-Resume-2023-2024"
-                rel="noopener noreferrer"
-                className="flex flex-row justify-center items-center text-center m-auto lg:w-[40%] w-[90%] p-4 rounded-xl font-semibold bg-[#6947ef] text-white hover:bg-[#6947ef] hover:text-[#0F0F0F] transition-all duration-500"
-               > <button id="downloadResume" value="download">Get My Resume</button>
-               <DownloadIcon className="text-[25px]" />
-               </a>
+               <DialogBox />
                 <Icons />
             </div>
             <div className="lg:w-[20%] w-[50%] float-right bg-transparent">
